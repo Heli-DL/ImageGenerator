f63fc6925a4df7f41213f58e679825876bde15a6
HeliDL <85070320+Heli-DL@users.noreply.github.com>
2023-12-20 18:25:31 +0200

    changed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,13 +12,11 @@ export const Navbar = () => {
   const logOut = async () => {
     await signOut(auth)
     navigator("#/login")
-    navigator("#/login")
   }
 
   return (
     <nav className="navbar navbar-dark navbar-expand-lg"  style={{backgroundColor: '#1f3540'}}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="#/">Image<span style={{color: '#CE5A67'}}>Generator</span></a>
         <a className="navbar-brand" href="#/">Image<span style={{color: '#CE5A67'}}>Generator</span></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
